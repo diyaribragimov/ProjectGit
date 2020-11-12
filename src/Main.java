e8747d987f70abf4afdaddbac679af633ad0628a
diyaribragimov <ibragimov2892@gmail.com>
2020-11-12 09:44:45 -0600

    working on nice message

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello Git Last Day");
+        // add some comment
+        // B21 is excited!
     }
 }
